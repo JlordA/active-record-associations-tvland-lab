7bc4bef1fbe59efe823654dabf19de5515e94ca4
JlordA <james.ardery@gmail.com>
2020-10-17 13:53:49 -0400

    finished

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    fullname = [self.first_name, self.last_name].join (' ')
+    fullname
+  end
+
+  def list_roles
+    # binding.pry
+    characters.collect do |character| 
+      "#{character.name} - #{character.show.name}"
+    end
+  end
 end
---
+++ db/migrate/20201016125016_create_characters.rb
@@ -0,0 +1,12 @@
+class CreateCharacters < ActiveRecord::Migration[6.0]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
+      t.string :catchphrase
+ 
+      t.timestamps
+    end
+  end
+end
